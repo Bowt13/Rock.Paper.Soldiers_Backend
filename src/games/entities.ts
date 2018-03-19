93ecf7246de5f44f4677c13ae1f3fc60ca360dec
nsch1 <nick.schooneman1@gmail.com>
2018-03-19 16:56:09 +0100

    Add game & player entities. Also add relations between all entities

--- src/games/entities.ts
+++ src/games/entities.ts
@@ -1,7 +1,25 @@
-import {BaseEntity, Column, Entity, PrimaryGeneratedColumn} from "typeorm";
+import {BaseEntity, Column, Entity, ManyToOne, OneToMany, PrimaryGeneratedColumn} from "typeorm";
+import User from "../users/entity";
 
 type Character = 'fighter' | 'mage' | 'cleric'
+type Status = 'pending' | 'started' | 'finished'
 
+@Entity()
+export class Game extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id?: number
+
+  @Column('text', {default: 'pending'})
+  status: Status
+
+  @Column('text', {nullable:true})
+  winner: string
+
+  @OneToMany(_ => Player, player => player.game, {eager:true})
+  players: Player[]
+
+}
 
 @Entity()
 export class Player extends BaseEntity {
@@ -9,7 +27,16 @@ export class Player extends BaseEntity {
   @PrimaryGeneratedColumn()
   id?: number
 
+  @Column('integer')
+  hp: number
+
   @Column('text')
   character: Character
+
+  @ManyToOne(_ => Game, game => game.players)
+  game: Game
+
+  @ManyToOne(_ => User, user => user.players)
+  user: User
 
 }
