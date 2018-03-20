a84bc27b8080efac93066235e4477148856fddf1
nsch1 <nick.schooneman1@gmail.com>
2018-03-20 17:01:54 +0100

    Incomplete but working patch route, need to add game logic

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,10 +1,15 @@
 import {
-  Authorized, BadRequestError, CurrentUser, Get, HttpCode, JsonController, Param,
+  Authorized, BadRequestError, Body, CurrentUser, ForbiddenError, Get, HttpCode, JsonController, NotFoundError, Param,
+  Patch,
   Post
 } from "routing-controllers";
 import User from "../users/entity";
 import {Game, Player} from "./entities";
 import {io} from "../index";
+
+interface attackType {
+  attackType: "melee" | "spell" | "ranged"
+}
 
 @JsonController()
 export default class GameController {
@@ -49,7 +54,7 @@ export default class GameController {
     const player = await Player.create({
       game,
       user,
-      character: 'fighter'
+      character: 'mage'
     }).save()
 
     io.emit('action', {
@@ -58,6 +63,42 @@ export default class GameController {
     })
 
     return player
+  }
+
+  @Authorized()
+  @Patch('/games/:id([0-9]+)')
+  async updateGame(
+    @CurrentUser() user: User,
+    @Param('id') gameId: number,
+    @Body() update: attackType
+  ) {
+    const game = await Game.findOneById(gameId)
+    if(!game) throw new NotFoundError('Game not found.')
+
+    const player = await Player.findOne({ user, game })
+
+
+    if(!player) throw new ForbiddenError('You are not in this game.')
+    if(game.status !== 'started') throw new BadRequestError('The game did not start yet or is already finished')
+    if(player.pendingMove) throw new BadRequestError('You already made a move, wait for the opponent')
+
+    const opponentId = game.players.filter(p => p.userId !== user.id)[0].userId
+    const opponent = await Player.findOne({ userId: opponentId })
+
+    if(!opponent) throw new BadRequestError("You somehow don't have an opponent and still got to this point..")
+
+    if(player.pendingMove && opponent.pendingMove) {
+      //Calculate if opponent or player wins this move
+      //Subtract damage from loser hp
+      //Set pendingMove for both players to null
+      //Save player entities
+      //Emit new game to the frontend
+      //return game
+    }
+    //set player.pendingMove to update.attackType
+    //save player with pending move
+    //Maybe emit some waiting status.
+    //return game
   }
 
   @Authorized()
