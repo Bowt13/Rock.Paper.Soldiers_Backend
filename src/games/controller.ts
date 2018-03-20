0ef8a8ea4ee3892da109542eb10691f84c8cd973
nsch1 <nick.schooneman1@gmail.com>
2018-03-20 13:50:07 +0100

    Add post game/id/players to join a game

--- src/games/controller.ts
+++ src/games/controller.ts
@@ -1,4 +1,4 @@
-import {Authorized, CurrentUser, HttpCode, JsonController, Post} from "routing-controllers";
+import {Authorized, BadRequestError, CurrentUser, HttpCode, JsonController, Param, Post} from "routing-controllers";
 import User from "../users/entity";
 import {Game, Player} from "./entities";
 import {io} from "../index";
@@ -25,7 +25,35 @@ export default class GameController {
       type: 'ADD_GAME',
       payload: game
     })
-    
+
     return game
+  }
+
+  @Authorized()
+  @Post('/games/:id([0-9]+)/players')
+  @HttpCode(201)
+  async joinGame(
+    @CurrentUser() user: User,
+    @Param('id') gameId: number
+  ) {
+    const game = await Game.findOneById(gameId)
+    if(!game) throw new BadRequestError('Game does not exist.')
+    if(game.status !== 'pending') throw new BadRequestError('Game has already started.')
+
+    game.status = 'started'
+    await game.save()
+
+    const player = await Player.create({
+      game,
+      user,
+      character: 'fighter'
+    })
+
+    io.emit('action', {
+      type: 'UPDATE_GAME',
+      payload: await Game.findOneById(game.id)
+    })
+
+    return player
   }
 }
